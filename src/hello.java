4041d85bbb5268b65fc4935b9a681209e9e068a1
Kevin Fairbanks <kevin@kevinfairbanks.com>
2015-10-31 17:59:07 -0400

    Create hello.java

---
+++ src/hello.java
@@ -0,0 +1,23 @@
+/**
+ * Intro2Java
+ * Part 1 - Hello World
+ *
+ * @package   Intro2Java
+ * @copyright Copyright (c) 2015, Kevin Fairbanks (http://kevinfairbanks.com)
+ * @author    Kevin Fairbanks (https://github.com/KevinFairbanks)
+ * @license   MIT License (http://opensource.org/licenses/MIT)
+ * @link      http://kevinfairbanks.com
+ */
+
+/**
+ * Hello World - an intro2java program
+ * @return string
+ */
+public class Hello 
+{
+    public static void main(String[] args)
+    {
+        // Output to console Hello World!
+        System.out.println("Hello World!");
+    }
+}
